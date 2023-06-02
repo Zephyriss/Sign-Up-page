d2db16a87ea517a420f70304cd6738293489a3e1
Zephyriss <124293404+Zephyriss@users.noreply.github.com>
2023-06-02 13:28:07 +0200

    Delete sign up page directory

--- sign up page/main.js
+++
@@ -1,7 +0,0 @@
-let darkmode = document.querySelector('.switch')
-
-darkmode.onclick = function() {
-    document.body.classList.toggle("dark-theme")
-    darkmode.classList.toggle('active')
-    content.classList.toggle('night')
-}
